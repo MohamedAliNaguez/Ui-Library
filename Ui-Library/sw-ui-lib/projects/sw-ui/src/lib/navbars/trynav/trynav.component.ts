57977fcd541372999db3b4695e52539217740238
unknown <mohamedali.naguez@esprit.tn>
2024-07-29 11:54:29 +0100

    changed path

---
+++ Ui-Library/sw-ui-lib/projects/sw-ui/src/lib/navbars/trynav/trynav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-trynav',
+  templateUrl: './trynav.component.html',
+  styleUrls: ['./trynav.component.css']
+})
+export class TrynavComponent {
+
+}
