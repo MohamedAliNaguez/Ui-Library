f4b399cba5f2629e68a6ea1f1ca15fe43699aead
unknown <mohamedali.naguez@esprit.tn>
2024-07-31 15:34:59 +0100

    test button

---
+++ sw-ui-lib/projects/sw-ui/src/lib/buttons/testbutton/testbutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-testbutton',
+  templateUrl: './testbutton.component.html',
+  styleUrls: ['./testbutton.component.css']
+})
+export class TestbuttonComponent {
+
+}
