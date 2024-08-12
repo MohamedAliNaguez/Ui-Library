ca0f7a4d66697e314d3d65d4194037d9b9a4e268
unknown <mohamedali.naguez@esprit.tn>
2024-08-12 13:53:08 +0100

    added category to meta data

--- sw-ui-lib/projects/sw-ui/src/lib/components-metadata.ts
+++ sw-ui-lib/projects/sw-ui/src/lib/components-metadata.ts
@@ -4,132 +4,151 @@ export const componentsMetadata = [
     "selector": "lib-butbut",
     "description": "A description for lib-butbut",
     "usage": "<lib-butbut></lib-butbut>",
-    "path": "buttons/butbut/butbut.component.ts"
+    "path": "buttons/butbut/butbut.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-newbutton",
     "selector": "lib-newbutton",
     "description": "A description for lib-newbutton",
     "usage": "<lib-newbutton></lib-newbutton>",
-    "path": "buttons/newbutton/newbutton.component.ts"
+    "path": "buttons/newbutton/newbutton.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-newbutton1",
     "selector": "lib-newbutton1",
     "description": "A description for lib-newbutton1",
     "usage": "<lib-newbutton1></lib-newbutton1>",
-    "path": "buttons/newbutton1/newbutton1.component.ts"
+    "path": "buttons/newbutton1/newbutton1.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-nouveaubutton",
     "selector": "lib-nouveaubutton",
     "description": "A description for lib-nouveaubutton",
     "usage": "<lib-nouveaubutton></lib-nouveaubutton>",
-    "path": "buttons/nouveaubutton/nouveaubutton.component.ts"
+    "path": "buttons/nouveaubutton/nouveaubutton.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-sw-but",
     "selector": "lib-sw-but",
     "description": "A description for lib-sw-but",
     "usage": "<lib-sw-but></lib-sw-but>",
-    "path": "buttons/sw-but/sw-but.component.ts"
+    "path": "buttons/sw-but/sw-but.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-sw-primary-button",
     "selector": "lib-sw-primary-button",
     "description": "A description for lib-sw-primary-button",
     "usage": "<lib-sw-primary-button></lib-sw-primary-button>",
-    "path": "buttons/sw-primary-button/sw-primary-button.component.ts"
+    "path": "buttons/sw-primary-button/sw-primary-button.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-sw-secondary-button",
     "selector": "lib-sw-secondary-button",
     "description": "A description for lib-sw-secondary-button",
     "usage": "<lib-sw-secondary-button></lib-sw-secondary-button>",
-    "path": "buttons/sw-secondary-button/sw-secondary-button.component.ts"
+    "path": "buttons/sw-secondary-button/sw-secondary-button.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-sw-success-button",
     "selector": "lib-sw-success-button",
     "description": "A description for lib-sw-success-button",
     "usage": "<lib-sw-success-button></lib-sw-success-button>",
-    "path": "buttons/sw-success-button/sw-success-button.component.ts"
+    "path": "buttons/sw-success-button/sw-success-button.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-testbutton",
     "selector": "lib-testbutton",
     "description": "A description for lib-testbutton",
     "usage": "<lib-testbutton></lib-testbutton>",
-    "path": "buttons/testbutton/testbutton.component.ts"
+    "path": "buttons/testbutton/testbutton.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-trybutt",
     "selector": "lib-trybutt",
     "description": "A description for lib-trybutt",
     "usage": "<lib-trybutt></lib-trybutt>",
-    "path": "buttons/trybutt/trybutt.component.ts"
+    "path": "buttons/trybutt/trybutt.component.ts",
+    "categoty": "buttons"
   },
   {
     "name": "lib-newnav",
     "selector": "lib-newnav",
     "description": "A description for lib-newnav",
     "usage": "<lib-newnav></lib-newnav>",
-    "path": "navbars/newnav/newnav.component.ts"
+    "path": "navbars/newnav/newnav.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-sw-centred-navbar",
     "selector": "lib-sw-centred-navbar",
     "description": "A description for lib-sw-centred-navbar",
     "usage": "<lib-sw-centred-navbar></lib-sw-centred-navbar>",
-    "path": "navbars/sw-centred-navbar/sw-centred-navbar.component.ts"
+    "path": "navbars/sw-centred-navbar/sw-centred-navbar.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-sw-right-aligned-navbar",
     "selector": "lib-sw-right-aligned-navbar",
     "description": "A description for lib-sw-right-aligned-navbar",
     "usage": "<lib-sw-right-aligned-navbar></lib-sw-right-aligned-navbar>",
-    "path": "navbars/sw-right-aligned-navbar/sw-right-aligned-navbar.component.ts"
+    "path": "navbars/sw-right-aligned-navbar/sw-right-aligned-navbar.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-sw-simple-navbar",
     "selector": "lib-sw-simple-navbar",
     "description": "A description for lib-sw-simple-navbar",
     "usage": "<lib-sw-simple-navbar></lib-sw-simple-navbar>",
-    "path": "navbars/sw-simple-navbar/sw-simple-navbar.component.ts"
+    "path": "navbars/sw-simple-navbar/sw-simple-navbar.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-trynav",
     "selector": "lib-trynav",
     "description": "A description for lib-trynav",
     "usage": "<lib-trynav></lib-trynav>",
-    "path": "navbars/trynav/trynav.component.ts"
+    "path": "navbars/trynav/trynav.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-trynav22",
     "selector": "lib-trynav22",
     "description": "A description for lib-trynav22",
     "usage": "<lib-trynav22></lib-trynav22>",
-    "path": "navbars/trynav22/trynav22.component.ts"
+    "path": "navbars/trynav22/trynav22.component.ts",
+    "categoty": "navbars"
   },
   {
     "name": "lib-sw-basic-dropdown-list",
     "selector": "lib-sw-basic-dropdown-list",
     "description": "A description for lib-sw-basic-dropdown-list",
     "usage": "<lib-sw-basic-dropdown-list></lib-sw-basic-dropdown-list>",
-    "path": "drop-lists/sw-basic-dropdown-list/sw-basic-dropdown-list.component.ts"
+    "path": "drop-lists/sw-basic-dropdown-list/sw-basic-dropdown-list.component.ts",
+    "categoty": "drop-lists"
   },
   {
     "name": "lib-sw-icons-dropdown-list",
     "selector": "lib-sw-icons-dropdown-list",
     "description": "A description for lib-sw-icons-dropdown-list",
     "usage": "<lib-sw-icons-dropdown-list></lib-sw-icons-dropdown-list>",
-    "path": "drop-lists/sw-icons-dropdown-list/sw-icons-dropdown-list.component.ts"
+    "path": "drop-lists/sw-icons-dropdown-list/sw-icons-dropdown-list.component.ts",
+    "categoty": "drop-lists"
   },
   {
     "name": "lib-sw-styled-dropdown-list",
     "selector": "lib-sw-styled-dropdown-list",
     "description": "A description for lib-sw-styled-dropdown-list",
     "usage": "<lib-sw-styled-dropdown-list></lib-sw-styled-dropdown-list>",
-    "path": "drop-lists/sw-styled-dropdown-list/sw-styled-dropdown-list.component.ts"
+    "path": "drop-lists/sw-styled-dropdown-list/sw-styled-dropdown-list.component.ts",
+    "categoty": "drop-lists"
   }
 ];
