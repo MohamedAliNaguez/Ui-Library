8b42022f9cdc3c48f36709add9f44ac0a1266519
unknown <mohamedali.naguez@esprit.tn>
2024-08-12 10:14:51 +0100

    sw-but

---
+++ sw-ui-lib/projects/sw-ui/src/lib/buttons/sw-but/sw-but.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sw-but',
+  templateUrl: './sw-but.component.html',
+  styleUrls: ['./sw-but.component.css']
+})
+export class SwButComponent {
+
+}
