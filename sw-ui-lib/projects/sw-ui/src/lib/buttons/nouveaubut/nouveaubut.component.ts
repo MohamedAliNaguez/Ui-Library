3ff243b5e33eb06454ae691685e0a718929e0e29
unknown <mohamedali.naguez@esprit.tn>
2024-08-13 12:29:20 +0100

    nouvbut

---
+++ sw-ui-lib/projects/sw-ui/src/lib/buttons/nouveaubut/nouveaubut.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-nouveaubut',
+  templateUrl: './nouveaubut.component.html',
+  styleUrls: ['./nouveaubut.component.css']
+})
+export class NouveaubutComponent {
+
+}
