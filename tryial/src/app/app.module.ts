7626d6be32745b1d9390f24d77b0b3d318bf4dc2
unknown <mohamedali.naguez@esprit.tn>
2024-08-19 09:40:45 +0100

    new button

--- tryial/src/app/app.module.ts
+++ tryial/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShowcaseComponent } from './showcase/showcase.component'
 import { SwUiModule } from 'dalynaguez-sw-ui';
-import { ComponentListComponent } from './component-list/component-list.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    ShowcaseComponent,
-    ComponentListComponent ,
+  
     
   ],
   imports: [
