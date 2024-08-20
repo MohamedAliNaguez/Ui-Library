f1e8ccac17eb4b3539ecf9cce2ddb2c2533cfd99
unknown <mohamedali.naguez@esprit.tn>
2024-08-20 10:54:06 +0100

    new button

---
+++ sw-ui-lib/projects/sw-ui/src/lib/buttons/newbnewbutton/newbnewbutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-newbnewbutton',
+  templateUrl: './newbnewbutton.component.html',
+  styleUrls: ['./newbnewbutton.component.css']
+})
+export class NewbnewbuttonComponent {
+
+}
